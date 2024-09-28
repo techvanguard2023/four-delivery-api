8a43e859e3fe810144e1adbf5f3359f825206551
Tech Vanguard <suporte@techvanguard.com.br>
2024-09-28 10:56:40 -0300

    removendo comentario

--- app/Http/Controllers/Admin/ViaCepController.php
+++ app/Http/Controllers/Admin/ViaCepController.php
@@ -10,7 +10,6 @@ class ViaCepController extends Controller
 {
     public function search($zipCode)
     {
-        // Validar formato do CEP
         if (!preg_match('/^[0-9]{8}$/', $zipCode)) {
             return response()->json(['error' => 'CEP inválido'], 400);
         }
@@ -18,11 +17,9 @@ class ViaCepController extends Controller
         $url = "https://viacep.com.br/ws/$zipCode/json/";
         $response = Http::get($url);
 
-        // Verificar se a resposta foi bem-sucedida
         if ($response->successful()) {
             $data = $response->json();
 
-            // Verificar se o CEP foi encontrado
             if (isset($data['erro']) && $data['erro'] === true) {
                 return response()->json(['error' => 'CEP não encontrado'], 404);
             }
@@ -30,7 +27,6 @@ class ViaCepController extends Controller
             return $data;
         }
 
-        // Retornar erro se a requisição falhar
         return response()->json(['error' => 'Erro ao consultar o CEP'], 500);
     }
 }
