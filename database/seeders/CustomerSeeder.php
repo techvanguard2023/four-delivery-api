912e92ca156940b7677066bf459c6e64ed8e7ab6
Tech Vanguard <suporte@techvanguard.com.br>
2025-04-23 20:41:43 -0300

    ajusta customerseeder

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -16,91 +16,91 @@ class CustomerSeeder extends Seeder
         Customer::create([
             'name' => 'João da Silva',
             'phone' => '1234567890',
-            'is_whatsapp' => fake()->boolean,
+            'is_whatsapp' => 1,
             'company_id' => 1,
         ]);
 
         Customer::create([
             'name' => 'Maria Souza',
             'phone' => '9876543210',
-            'is_whatsapp' => fake()->boolean,
+            'is_whatsapp' => 1,
             'company_id' => 2,
         ]);
 
         Customer::create([
             'name' => 'Carlos Pereira',
             'phone' => '5555755555',
-            'is_whatsapp' => fake()->boolean,
+            'is_whatsapp' => 1,
             'company_id' => 1,
         ]);
 
         Customer::create([
             'name' => 'Ana Costa',
             'phone' => '1111111111',
-            'is_whatsapp' => fake()->boolean,
+            'is_whatsapp' => 1,
             'company_id' => 2,
         ]);
 
         Customer::create([
             'name' => 'Pedro Santos',
             'phone' => '2222222222',
-            'is_whatsapp' => fake()->boolean,
+            'is_whatsapp' => 1,
             'company_id' => 1,
         ]);
 
         Customer::create([
             'name' => 'Luiza Oliveira',
             'phone' => '3333333333',
-            'is_whatsapp' => fake()->boolean,
+            'is_whatsapp' => 1,
             'company_id' => 2,
         ]);
 
         Customer::create([
             'name' => 'Marcelo Mendes',
             'phone' => '4444444444',
-            'is_whatsapp' => fake()->boolean,
+            'is_whatsapp' => 1,
             'company_id' => 1,
         ]);
 
         Customer::create([
             'name' => 'Juliana Ferreira',
             'phone' => '5555555555',
-            'is_whatsapp' => fake()->boolean,
+            'is_whatsapp' => 1,
             'company_id' => 2,
         ]);
 
         Customer::create([
             'name' => 'Rafaela Gomes',
             'phone' => '6666666666',
-            'is_whatsapp' => fake()->boolean,
+            'is_whatsapp' => 1,
             'company_id' => 1,
         ]);
 
         Customer::create([
             'name' => 'André Sousa',
             'phone' => '7777777777',
-            'is_whatsapp' => fake()->boolean,
+            'is_whatsapp' => 1,
             'company_id' => 2,
         ]);
 
         Customer::create([
             'name' => 'Carla Ferreira',
             'phone' => '8888888888',
-            'is_whatsapp' => fake()->boolean,
+            'is_whatsapp' => 1,
             'company_id' => 1,
         ]);
 
         Customer::create([
             'name' => 'Fernando Rodrigues',
             'phone' => '9999999999',
-            'is_whatsapp' => fake()->boolean,
+            'is_whatsapp' => 1,
             'company_id' => 2,
         ]);
 
         Customer::create([
             'name' => 'Mariana Silva',
             'phone' => '1010101010',
-            'is_whatsapp' => fake()->boolean,
+            'is_whatsapp' => 1,
             'company_id' => 1,
         ]);
     }
